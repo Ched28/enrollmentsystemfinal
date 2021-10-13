046f8c6cadbc10c5752265c48e03978e28484c10
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-13 17:53:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/styles.css">
-    <link rel="stylesheet" href="/styles/all.css">
+    <link rel="stylesheet" href="../styles/styles.css/">
+    <link rel="stylesheet" href="../styles/all.css/">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
     </style>
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div class="container">
-        <iframe src="/components/header.php" frameborder="0"></iframe>
+        <iframe src="../components/header.php/"></iframe>
     <a href="/login.html" class="fixed-button login-btn"> <i class="fas fa-user"></i> &nbsp; Log In</a>
           
           <main>
