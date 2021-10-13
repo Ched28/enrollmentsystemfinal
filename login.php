168c02a2cf81cb3d5314f96940c3a2e25bbb7373
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-13 19:04:48 +0800

    created log in php design

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystem/components/header.php"); ?>
+
+<main>
+        
+        <div class="loginform">
+        <div class="form-bg">
+        <form>
+            <h1>Log In</h1>
+            <input type="text" name="" placeholder="Student ID: XX-XXXX" class="one-line"> <br><br>
+            <input type="password" name="" placeholder="Password" class="one-line"> <br><br>
+            <button type="submit"  name="" class="login-btn-login"> Log In </button>
+            <button type="button" style="background-color: #e82048;" onclick="location.href='index.html'" class="login-btn-login"> Back to Home </button>
+        </form>
+    </div>
+</div>        
+    </main>
+
+<?php include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystem/components/footer.php"); ?>
