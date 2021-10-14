0b8810a708198a3632b8aff1956f5b2161e5bc1a
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-14 10:37:47 +0800

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
             <input type="text" name="" placeholder="Student ID: XX-XXXX" class="one-line"> <br><br>
             <input type="password" name="" placeholder="Password" class="one-line"> <br><br>
             <button type="submit"  name="" class="login-btn-login"> Log In </button>
-            <button type="button" style="background-color: #e82048;" onclick="location.href='index.html'" class="login-btn-login"> Back to Home </button>
+            <button type="button" style="background-color: #e82048;" onclick="location.href='index.php'" class="login-btn-login"> Back to Home </button>
         </form>
     </div>
 </div>        
