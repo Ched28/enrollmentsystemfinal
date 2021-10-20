998cd600bbf8f5181fd9ca21a3d69c2746332e53
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-20 20:38:53 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+include("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/connections/dbcon.php");
+
+$id = 0;
+if(isset($_POST['submit'])){
+    $examcode = $_POST['examcode'];
+    $examdate = $_POST['examdate'];
+    if(!empty($examcode) && !empty($examdate))
+    {
+        $query ="SELECT * FROM `studentexamresultstemp` WHERE ExamNo = '$examcode' AND ExamDate= '$examdate' LIMIT 1";
+        $result = mysqli_query($con, $query);
+
+        if($result && mysqli_num_rows($result) > 0)
+            {
+                $user_data = mysqli_fetch_assoc($result);
+                while ($row = mysqli_fetch_array($result)) { 
+                    
+                    $id = $row['id'];
+                    $code = $row['ExamCode'];
+                    echo $id;
+                    echo $code;
+                }
+            }
+        echo print_r($result);
+
+    }    
+}
+
+?>
+
+<form method="post">
+   <input type="text" name="examcode">
+   <input type="date" name="examdate">
+   <input type="submit">
+</form>
