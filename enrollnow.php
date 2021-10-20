b7df5571497465f94ef2e71e50eaf55125d248c9
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-20 18:57:10 +0800

    updateeee

--- enrollnow.php
+++ enrollnow.php
@@ -2,6 +2,28 @@
 include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/components/header.php"); 
 include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/connections/dbcon.php");
 
+if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$examcode = $_POST['examcode'];
+		$examdate = $_POST['examdate'];
+        if(!empty($examcode) && !empty($examdate))
+		{
+            $query ="SELECT * FROM `studentexamresultstemp` WHERE ExamNo = '$examcode' AND ExamDate= '$examdate' LIMIT 1";
+			$result = mysqli_query($con, $query);
+
+            if($result && mysqli_num_rows($result) > 0)
+				{
+                    $user_data = mysqli_fetch_assoc($result);
+                    echo "<script>
+                    
+                    location.replace('mail.php');
+                    
+                    </script>";
+                }
+
+        }
+    }
 ?>
 
 <a href="/login.html" class="fixed-button login-btn"> <i class="fas fa-user"></i> &nbsp; Log In</a>
@@ -13,8 +35,8 @@ include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/connections/dbcon.ph
                     <h1>Please Enter Your Exam Code and Date</h1>
                     <input type="text" name="examcode" placeholder="Exam Code" class="one-line"> <br><br>
                     <input type="date" name="examdate"  class="one-line"> <br><br>
-                    <button type="button" name="" onclick="location.href='mail.php'">Proceed </button> <!--- make this a function :)-->
-                    <button type="submit" name="" style="background-color: #3366CC"> Apply as Student </button> 
+                    <button type="submit" name="" onclick="location.href='mail.php'">Proceed </button> <!--- make this a function :)-->
+                    <button type="button" name="" style="background-color: #3366CC"> Apply as Student </button> 
                     
                 </form>
             </div>
