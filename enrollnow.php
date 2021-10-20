3c8f175bf7d026a795bd84b83c647221070ee12b
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-20 20:46:50 +0800

    Update enrollnow.php

--- enrollnow.php
+++ enrollnow.php
@@ -10,7 +10,7 @@ include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/components/header.ph
                 <form method="POST">
                     <h1>Please Enter Your Exam Code and Date</h1>
                     <input type="text" name="examcode" placeholder="Exam Code" class="one-line"> <br><br>
-                    <input type="date" name="examdate"  class="one-line"> <br><br>
+                    <input type="text" name="examdate"  class="one-line"> <br><br>
                     <input type="submit">Proceed </input> <!--- make this a function :)-->
                     <button type="button" name="" style="background-color: #3366CC"> Apply as Student </button> 
                     
