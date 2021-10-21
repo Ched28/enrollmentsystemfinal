6e6ab78380bc0498e05d6bd4325b87b2ade47e53
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-22 01:28:56 +0800

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -1,6 +1,33 @@
 <?php
 session_start();
 include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/components/header.php"); 
+$id = $_GET['id'];
+function hideEmailAddress($email)
+{
+    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        list($first, $last) = explode('@', $email);
+        $first = str_replace(substr($first, '3'), str_repeat('*', strlen($first)-3), $first);
+        $last = explode('.', $last);
+        $last_domain = str_replace(substr($last['0'], '1'), str_repeat('*', strlen($last['0'])-1), $last['0']);
+        $hideEmailAddress = $first.'@'.$last_domain.'.'.$last['1'];
+        return $hideEmailAddress;
+    }
+}
+
+$query = "select * from `studentexamresultstemp` where id = $id LIMIT 1";
+$result = mysqli_query($con, $query);
+if($result){
+    while ($row = mysqli_fetch_array($result)){
+        
+        $email1 = $row['Email'];
+        $_SESSION['email'] = $email1;
+
+
+}
+}
+$emailfinal = $_SESSION['email'];
+
 ?>
 
 <a href="/login.html" class="fixed-button login-btn"> <i class="fas fa-user"></i> &nbsp; Log In</a>
@@ -12,6 +39,7 @@ include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/components/header.ph
                     <h4>Congratulations! You are qualified to be a student in Quezon City University</h4>
 
                     <p>A <em><span style="color: #e82048"> temporary code </span></em> has been sent to your respective email.</p>
+                    <p> <em> <?php echo hideEmailAddress($emailfinal) ?> </em></p>
                     <form>
                     <input type="text" placeholder="Exam Code" class="one-line"> <br><br>
                     <input type="password" placeholder="Verification Code" class="one-line"> <br> <br>
