798d1b8c0ea999a9c76e6cfba9b9e9d12ae9f7c6
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-23 15:39:46 +0800

    adding admin ui

---
+++ admin/admin-page-crud.php
@@ -0,0 +1,412 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>QCU | Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+	</style>
+</head>
+<body>
+	
+
+
+<div class="nav">
+	<div class="nav-content">
+
+	<table>
+		<tr>
+			<td ><img class="qculogo"  src="qcu.png"></td>
+			
+			<td >
+				<h1 class="nav-qcuname"> Quezon City University <span style="font-size:15px;font-weight: normal;">| Admin</span></h1>
+			</td>
+			
+			<td >
+				<input id="srch-bar" type="text" name="search" placeholder="search.."><button id="srch-bttn">search</button>
+			</td>
+			<td >
+				<h1 > <p class="nav-date"><script > document.write(new Date().toDateString()); </script></p></h1>
+			</td>
+			
+			<td >
+				<a  href=""> <button class="login-btn">Login</button></a>
+			
+			</td>
+		
+		</tr>
+	</table>
+
+	</div>	
+
+</div>
+<center>
+<div class="tbls">
+<h2>Personal Information</h2>
+<table class="personal-info">
+
+			<th class="tbl-head" >ID</th>
+			<th class="tbl-head" >NAME</th>
+			<th class="tbl-head" >BIRTHDAY</th>
+			<th class="tbl-head" >AGE</th>
+			<th class="tbl-head" >PLACE OF BIRTH</th>
+			<th class="tbl-head" >CIVIL STATUS</th>
+			<th class="tbl-head" >CITIZENSHIP</th>
+			<th class="tbl-head" >MOBILE NUMBER</th>
+			<th class="tbl-head" >EMAIL ADDRESS</th>
+			<th class="tbl-head" >PERMANENT ADDRESS</th>
+			<th class="tbl-head" >PROVINCIAL ADDRESS</th>
+			<th class="tbl-head" >DISTRICT NO.</th>
+			<th class="tbl-head" >ZIPCODE</th>
+			
+			<th class="tbl-head"  colspan="2">ACTION</th>
+
+			<tr>
+				<!--Here goes the list of data-->
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata"><button class="tbl-btn">EDIT</button><button class="tbl-btn">DELETE</button></td>
+			</tr>
+</table>
+<br>
+<h2>Academic Information</h2>
+<table class="academic-info">
+
+			<th class="tbl-head" >NAME OF SCHOOL (LAST ATTENDED)</th>
+			<th class="tbl-head" >SCHOOL ADDRESS</th>
+			<th class="tbl-head" >YEAR LEVEL / COURSE</th>
+			<th class="tbl-head" >SCHOOL SECTOR</th>
+			<th class="tbl-head" >HIGHEST ATTAINED YEAR LEVEL</th>
+		
+			<th class="tbl-head"  colspan="2">ACTION</th>
+
+			<tr>
+				<!--Here goes the list of data-->
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata"><td class="tdata"><button class="tbl-btn">EDIT</button><button class="tbl-btn">DELETE</button></td>
+				</td>
+				
+			</tr>
+</table>
+
+<br>
+<h2>Emergency Information</h2>
+<table class="emergency-info">
+
+			<th class="tbl-head" >NAME OF GUARDIAN</th>
+			<th class="tbl-head" >CONTACT NUMBER</th>
+			<th class="tbl-head" >HOME ADDRESS</th>
+			<th class="tbl-head"  colspan="2">ACTION</th>
+
+			<tr>
+				<!--Here goes the list of data-->
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata"><button class="tbl-btn">EDIT</button><button class="tbl-btn">DELETE</button></td>
+				
+				
+
+				
+			</tr>
+</table>
+
+<br>
+<h2>Enrollment Information</h2>
+<table class="enrollment-info">
+
+			<th class="tbl-head" >COURSE</th>
+			<th class="tbl-head" >YEAR LEVEL</th>
+			<th class="tbl-head" >ENROLLMENT CODE</th>
+			<th class="tbl-head" >SUBJECT#1</th>
+			<th class="tbl-head" >SUBJECT#2</th>
+			<th class="tbl-head" >SUBJECT#3</th>
+			<th class="tbl-head" >SUBJECT#4</th>
+			<th class="tbl-head" >SUBJECT#5</th>
+			<th class="tbl-head" >SUBJECT#6</th>
+			<th class="tbl-head" >SUBJECT#7</th>
+			<th class="tbl-head" >SUBJECT#8</th>
+			
+			<th class="tbl-head"  colspan="2">ACTION</th>
+
+			<tr>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata">..</td>
+				<td class="tdata"><button class="tbl-btn">EDIT</button><button class="tbl-btn">DELETE</button></td>
+				
+			</tr>
+</table>
+</center>
+</div>
+
+<div class="crud-form">
+	<center>
+		<form>
+			<label>Personal Information</label> <br>
+			<input type="" name=""  placeholder="ID "> <br>	
+			<input type="" name=""  placeholder="NAME ">	<br>
+			<input type="" name=""  placeholder="BIRTHDAY ">	<br>
+			<input type="" name=""  placeholder="AGE ">	<br>
+			<input type="" name=""  placeholder="PLACE OF BIRTH ">	<br>
+			<input type="" name=""  placeholder="CIVIL STATUS ">	<br>
+			<input type="" name=""  placeholder="CITIZENSHIP ">	<br>
+			<input type="" name=""  placeholder="MOBILE NUMBER "> <br>
+			<input type="" name=""  placeholder="EMAIL ADDRESS ">	<br>
+			<input type="" name=""  placeholder="PERMANENT ADDRESS ">	<br>
+			<input type="" name=""  placeholder="PROVINCIAL ADDRESS ">	<br>
+			<input type="" name=""  placeholder="DISTRICT NO. "> <br>
+			<input type="" name=""  placeholder="ZIPCODE "> <br>
+			<label>Academic Information</label><br>
+			<input type="" name="" placeholder="ID "><br>
+			<input type="" name="" placeholder="NAME OF SCHOOL (LAST ATTENDED) "><br>
+			<input type="" name="" placeholder="SCHOOL ADDRESS "><br>
+			<input type="" name="" placeholder="YEAR LEVEL / COURSE "><br>
+			<input type="" name="" placeholder="SCHOOL SECTOR "><br>
+			<input type="" name="" placeholder="HIGHEST ATTAINED YEAR LEVEL "><br>
+			<label>Emergency Information</label><br>
+			<input type="" name="" placeholder="ID "><br>
+			<input type="" name="" placeholder="NAME OF GUARDIAN "><br>
+			<input type="" name="" placeholder="CONTACT NUMBER "><br>
+			<input type="" name="" placeholder="HOME ADDRESS "><br>
+			<label>Enrollment Information</label><br>
+			<input type="" name="" placeholder="ID "><br>
+			<input type="" name="" placeholder="COURSE"><br>
+			<input type="" name="" placeholder="YEAR LEVEL "><br>
+			<input type="" name="" placeholder="ENROLLMENT CODE "><br>
+			<input type="" name="" placeholder="SUBJECT1 "><br>
+			<input type="" name="" placeholder="SUBJECT2 "><br>
+			<input type="" name="" placeholder="SUBJECT3 "><br>
+			<input type="" name="" placeholder="SUBJECT4 "><br>
+			<input type="" name="" placeholder="SUBJECT5 "><br>
+			<input type="" name="" placeholder="SUBJECT6 "><br>
+			<input type="" name="" placeholder="SUBJECT7 "><br>
+			<input type="" name="" placeholder="SUBJECT8 "><br>
+
+			<button class="updt-butn">UPDATE</button>
+
+
+
+
+			
+
+		</form>
+		</center>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<style type="text/css">
+	h2{
+		margin-top: 50px;
+	}
+
+	.tbls{
+		text-align: center;
+		margin-left: 400px;
+		margin-top: 70px;
+		}
+
+	.nav{
+		font-family: 'Poppins', sans-serif;
+		background-color: #002347 ;
+		color: white;
+
+		width: 2000px;
+		padding: 2px;
+		margin-top: -75px;
+		margin-left: -10px;
+		height: 70px;
+		position: fixed;
+
+		}
+	.crud-form{
+		background-color:#bad7ff;
+		width: 200px;
+		margin-top: -900px;
+		position: inherit;
+		padding: 50px;
+	}
+	.updt-butn{
+		width: 200px;
+		margin-top: 20px;
+		background-color: #002347 ;
+		color: white;
+		border: none;
+	}
+	body{
+		background-color: #dedede;
+		font-family: 'Poppins', sans-serif;
+		}
+
+	.tdata{
+		background-color: white;
+		border: solid black 2px;
+		text-align: center;
+	}
+	.tbl-btn{
+		width: 150px;
+		background-color:#bad7ff;
+		border: none;
+	}
+	th{
+		background-color: #002347 ;
+		color: white;
+		font-family: 'Poppins', sans-serif;
+		padding-right: 10px;
+		padding-left: 10px;
+		width:300px;
+		text-align: center;
+		}
+
+
+	.nav-content{
+		font-size: 11px;
+		padding-left: 50px;
+		padding-top: 3px;
+		}
+	.qculogo{
+		height: 45x;
+		width: 45px;
+		
+
+		}
+	.nav-date{
+		margin-left:100px;
+		font-size: 15px;
+		}
+	.login-btn{
+		margin-left: 25px;
+		font-family: 'Poppins', sans-serif;
+		width: 75px;
+		height: 25px;
+		border: none;
+		background-color: white;
+		border-radius: 5px;
+		}
+	.nav-qcuname{
+		margin-left: 25px;
+		margin-right:150px;
+		font-family: 'Poppins', sans-serif;
+		text-align: center;
+		color: white;
+
+		}
+	#nav-link{
+		color: white;
+		text-decoration: none;
+		font-size: 15px;
+		}
+	.top-panel-tbl{
+		width:max;
+		height:50px;
+		text-align: center;
+		margin-top: 50px;
+		
+		}
+	#top-panel-btn{
+		font-family: 'Poppins', sans-serif;
+		background-color: #002347 ;
+		color: white;
+		width: 200px;
+		height: 50px;
+		border: none;
+		border-radius: 5px;
+		}
+	#srch-bar{
+		width: 250px;
+		height: 25px;
+		border: none;
+		}
+	#srch-bttn{
+		height: 27px;
+		background-color:#bad7ff;
+		font-family: 'Poppins', sans-serif;
+		font-size: 15px;
+		border: none;
+
+		}
+	.enroll-btn{
+		background-color:#bad7ff;
+		font-family: 'Poppins', sans-serif;
+		font-size: 15px;
+		font-weight: bold;
+		
+		width: 150px;
+		height: 50px;
+		border: none;
+		position: fixed;
+		margin-top: 350px;}
+
+	.body-content{
+		background-color: white;
+		width:900px;
+		height: 600px;
+		border: solid 1px #b8b8b8;
+		border-radius: 4px;
+		margin-top: 25px;
+		}
+	.content-title{
+		margin-top: 30px;
+		font-size: 30px; 
+		font-weight: bold;
+		font-family: 'Poppins', sans-serif;
+		}
+	.content{
+		width:350px;
+		height:150px;
+		background-color: #e0e0e0;
+		text-align: justify;
+		font-size: 14px;
+		font-family: 'Poppins', sans-serif;
+		padding: 20px;
+
+		}
+	.qcu-gate{
+		position: static;
+		
+		width: 200px;
+		height: 200px;
+		}
+	.mayor {
+		width: 200px;
+		height: 200px;
+		}
+	
+</style>
+</body>
+</html>
