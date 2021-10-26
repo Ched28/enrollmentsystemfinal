7e2ba722520848d47e733a5665c2006121e28489
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-26 10:24:15 +0800

    Update test.php

--- test.php
+++ test.php
@@ -3,13 +3,18 @@
 
 
 
-for($a = 1;$a<=100;$a++){
-    $getstring = str_pad($a, 4, 0, STR_PAD_LEFT);
-    $id = "21-$getstring";
-    echo "$id <br>";
+//for($a = 1;$a<=100;$a++){
+ //   $getstring = str_pad($a, 4, 0, STR_PAD_LEFT);
+//    $id = "21-$getstring";
+//    echo "$id <br>";
 
 
-}
+//}
+
+
+$idint = 0001; 
+
+echo "21-$idint";
 
 
 ?>
