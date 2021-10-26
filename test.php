e98fd4aab0e5a166b1d911f1ce9a99f2b11f1c7e
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-26 10:21:25 +0800

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 
 
 for($a = 1;$a<=100;$a++){
-    $getstring = str_pad($a, 5, 0, STR_PAD_LEFT);
+    $getstring = str_pad($a, 4, 0, STR_PAD_LEFT);
     $id = "21-$getstring";
     echo $id "<br>";
 
