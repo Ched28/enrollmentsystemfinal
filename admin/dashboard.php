9ca891b812d04c695ad08189339a55cc9e07f58f
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-10-30 22:50:45 +0800

    add

---
+++ admin/dashboard.php
@@ -0,0 +1,29 @@
+<?php 
+$cssfile2 = "/enrollmentsystemfinal/styles/all.css";
+$iconsite = "/enrollmentsystemfinal/img/qcu.png";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo $cssfile2;?>" type="text/css">
+    <link rel="stylesheet" href="admin_styles.css" type="text/css">
+    <link rel="icon" href="<?php echo $iconsite;?>" type="image/icon type">
+    <title>QCU Admin Portal</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+    </style>
+</head>
+<body>
+    
+<div class="container">
+
+<nav>gfdgfdgfd</nav>
+<main>fgfdgfd g</main>
+<div class="content">fdgfdgfdgfdgfd</div>
+
+
+<?php include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/components/footer.php"); ?>
