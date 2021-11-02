13eb3545921e59d70e4ccd9be64d3445ee3255b5
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-11-03 00:46:43 +0800

    adddd

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,48 +1,6 @@
 <?php 
-$cssfile2 = "/enrollmentsystemfinal/styles/all.css";
-$iconsite = "/enrollmentsystemfinal/img/qcu.png";
-
+include_once("$_SERVER[DOCUMENT_ROOT]/enrollmentsystemfinal/admin/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo $cssfile2;?>" type="text/css">
-    <link rel="stylesheet" href="admin_styles.css" type="text/css">
-    <link rel="icon" href="<?php echo $iconsite;?>" type="image/icon type">
-    <title>QCU Admin Portal</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-    </style>
-</head>
-<body>
-    
-<div class="container">
-
-<nav>
-            <div class="logo">
-                <img class="qculogo"  src="<?php echo $iconsite;?>"> &nbsp;&nbsp;&nbsp;
-                    <h3>Quezon City University | <span style="font-size:15px;">  Admin Portal</span></h3>
-            </div>
-            <ul class="nav-list">
-                <li class="list-items">
-                <a href="../login.php" class="login-btn"> Log Out &nbsp; <i class="fas fa-sign-out-alt"></i></a>
-                </li>
-            </ul>
-
-</nav>
-<main>
-<ul class="side-list">
-    <li class="ul-list"> <i class="fas fa-user-shield"> &nbsp; </i>  Admin </li>
-    <li class="ul-list"><i class="fas fa-chart-line"></i> &nbsp; Dashboard </li>
-    <li class="ul-list"><i class="fas fa-list-ol"></i> &nbsp; List of Enrollees </li>
-    <li class="ul-list"><i class="far fa-folder"></i> &nbsp; Documents </li>
-    
-
-</ul>
-</main>
 <div class="content">
     <div class="content-main">
     <div class="content-date"> <h3> As of <script> document.write(new Date().toDateString()); </script> </h3></div>
