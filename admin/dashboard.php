527ac7f5ac5fc8a46d41b6b1d3441b5d787123ed
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-11-02 11:02:39 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -47,20 +47,41 @@ $iconsite = "/enrollmentsystemfinal/img/qcu.png";
     <div class="content-main">
     <div class="content-date"> <h3> As of <script> document.write(new Date().toDateString()); </script> </h3></div>
     <div class="content1">
-     <h1> 100 Passers  <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> 500 Examinees </span>
+     <h1> 100 Passers  <hr> </h1> 
+     <span style="font-size: 1.3em;padding: 0;margin: 0;"> 500 Examinees </span>
          </div>
-    <div class="content2"> <h1> 1000  <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> New Enrollees </span></div>
-    <div class="content3"> <h1> 500   <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> Officially Enrolled </span> </div>
+    <div class="content2">
+         <h1> 1000  <hr> </h1> 
+         <span style="font-size: 1.3em;padding: 0;margin: 0;"> New Enrollees </span>
+        </div>
+    <div class="content3">
+         <h1> 500   <hr> </h1> 
+         <span style="font-size: 1.3em;padding: 0;margin: 0;"> Officially Enrolled </span> 
+        </div>
     <div class="content4">
-     <h1> 100 Passers  <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> 500 Examinees </span>
+     <h1> 1000   <hr> </h1> 
+     <span style="font-size: 1.3em;padding: 0;margin: 0;"> Regular </span>
          </div>
-    <div class="content5"> <h1> 1000  <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> New Enrollees </span></div>
-    <div class="content6"> <h1> 500   <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> Officially Enrolled </span> </div>
-    <div class="content7">
-     <h1> 100 Passers  <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> 500 Examinees </span>
+    <div class="content5"> 
+        <h1> 1000  <hr> </h1> 
+        <span style="font-size: 1.3em;padding: 0;margin: 0;"> Transferees </span>
+    </div>
+    <div class="content6"> 
+        <h1> 500   <hr> </h1> 
+        <span style="font-size: 1.3em;padding: 0;margin: 0;"> Returnees </span> 
+    </div>
+  <!--  <div class="content7">
+     <h1> 100 Passers  <hr> </h1> 
+     <span style="font-size: 1.3em;padding: 0;margin: 0;"> 500 Examinees </span>
          </div>
-    <div class="content8"> <h1> 1000  <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> New Enrollees </span></div>
-    <div class="content9"> <h1> 500   <hr> </h1> <span style="font-size: 1.3em;padding: 0;margin: 0;"> Officially Enrolled </span> </div>
+    <div class="content8"> 
+        <h1> 1000  <hr> </h1> 
+        <span style="font-size: 1.3em;padding: 0;margin: 0;"> New Enrollees </span>
+    </div>
+    <div class="content9"> 
+        <h1> 500   <hr> </h1> 
+        <span style="font-size: 1.3em;padding: 0;margin: 0;"> Officially Enrolled </span> 
+    </div>-->
     </div>
 </div>
 
