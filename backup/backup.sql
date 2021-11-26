7b5b0f2014ba98e24383d02c6febe1e23a165216
JC Pablico <81672296+jcp-qcu@users.noreply.github.com>
2021-11-26 22:20:45 +0800

    Back up and Restore DB

---
+++ backup/backup.sql
@@ -0,0 +1,103 @@
+DROP TABLE subject;
+
+CREATE TABLE `subject` (
+  `SUBJ_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `SUBJ_CODE` varchar(30) NOT NULL,
+  `SUBJ_DESCRIPTION` varchar(255) NOT NULL,
+  `UNIT` int(2) NOT NULL,
+  `PRE_REQUISITE` varchar(30) NOT NULL DEFAULT 'None',
+  `COURSE_ID` int(11) NOT NULL,
+  `AY` varchar(30) NOT NULL,
+  `SEMESTER` varchar(20) NOT NULL,
+  PRIMARY KEY (`SUBJ_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=586 DEFAULT CHARSET=latin1;
+
+INSERT INTO subject VALUES("500","","","0","","0","","");
+INSERT INTO subject VALUES("501","\06\0?\0\0\0\0C\0a\0l\0i\0b\0r\0i\01","","0","","0","","");
+INSERT INTO subject VALUES("502","\0#\0#\00\0;\0\"\0? \"\0\0 \0\0-\0#\0","\0#\0#\00\0=\0\0\0\"\0? \"\0\0 \0#\0","0","\0#\0#\00\0?\0\0\0\"\0? \"\0\0 \0#\0","0","\0#\0#\00\0.\00\00\0I\0\0\"\0\"\0? \"\0\0 ","\0#\0#\00\0.\00\00\0;\0[\0R\0e");
+INSERT INTO subject VALUES("503","","","0","","0","","");
+INSERT INTO subject VALUES("504","","","0","","0","","");
+INSERT INTO subject VALUES("505","","","0","","0","","");
+INSERT INTO subject VALUES("506","\0\0\0\0\0\0\0\0\0\0\0\000_ ;_ *\0\0","","0","","0","","");
+INSERT INTO subject VALUES("507","","","0","","0","","");
+INSERT INTO subject VALUES("508","","","0","","0","","");
+INSERT INTO subject VALUES("509","","","0","","0","","");
+INSERT INTO subject VALUES("510","","","0","","0","","");
+INSERT INTO subject VALUES("511","","","0","","0","","");
+INSERT INTO subject VALUES("512","???(\0?\0\0\0\0\0\0\0\0\0\0?	\0C\0o\0m","","0","","0","","");
+INSERT INTO subject VALUES("513","","","0","","0","","");
+INSERT INTO subject VALUES("514","","","0","","0","","");
+INSERT INTO subject VALUES("515","","","0","","0","","");
+INSERT INTO subject VALUES("516","","","0","","0","","");
+INSERT INTO subject VALUES("517","\0?\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0M\0y\0S\0q\0l","","0","","0","","");
+INSERT INTO subject VALUES("518","?En7?Li?b??/?S???e???????\0\0","","0","","0","","");
+INSERT INTO subject VALUES("519","??e?.???|","???H?","0","?^?$j=?GW???)?E?+&","0","","");
+INSERT INTO subject VALUES("520","","","0","","0","","");
+INSERT INTO subject VALUES("521","","","0","","0","","");
+INSERT INTO subject VALUES("522","l!?Fl?8Fm???c	7??CL?0^Es","","0","","0","","");
+INSERT INTO subject VALUES("523","??v????$k??S?`???4?)??]?l#","]?A????&o???l????A?<MBS????Z??a?/???n?jah??W?H?O??	vx????Tp?J??? ??$??E????#g?????????R????W*4?v????j???K?N?!4Fe?????Qt?}|Q?`??+?K#???RT???re????<?U??j?S+4??~??v?fP??????w????t?c???????Z9","0","","0","","");
+INSERT INTO subject VALUES("524","","","0","","0","","");
+INSERT INTO subject VALUES("525","","","0","","0","","");
+INSERT INTO subject VALUES("526","","","0","","0","","");
+INSERT INTO subject VALUES("527","","","0","","0","","");
+INSERT INTO subject VALUES("528","","","0","","0","","");
+INSERT INTO subject VALUES("529","","","0","","0","","");
+INSERT INTO subject VALUES("530","","","0","","0","","");
+INSERT INTO subject VALUES("531","","","0","","0","","");
+INSERT INTO subject VALUES("532","\0\0\0-\0\0\0.\0\0\0/\0\0\00\0\0\01\0\0\0???????","","0","","0","","");
+INSERT INTO subject VALUES("533","\06\0?\0\0\0\0C\0a\0l\0i\0b\0r\0i\01","","0","","0","","");
+INSERT INTO subject VALUES("534","","","0","","0","","");
+INSERT INTO subject VALUES("535","","","0","","0","","");
+INSERT INTO subject VALUES("536","","","0","","0","","");
+INSERT INTO subject VALUES("537","\0\0\0\0\0\0\0\0\0\0\0\0\00\00\0_\0)\0\0","","0","","0","","");
+INSERT INTO subject VALUES("538","","","0","","0","","");
+INSERT INTO subject VALUES("539","","","0","","0","","");
+INSERT INTO subject VALUES("540","","","0","","0","","");
+INSERT INTO subject VALUES("541","","","0","","0","","");
+INSERT INTO subject VALUES("542","","","0","","0","","");
+INSERT INTO subject VALUES("543","???(\0?\0\0\0\0\0\0\0\0\0\0?	\0C\0o\0m","","0","","0","","");
+INSERT INTO subject VALUES("544","","","0","","0","","");
+INSERT INTO subject VALUES("545","","","0","","0","","");
+INSERT INTO subject VALUES("546","","","0","","0","","");
+INSERT INTO subject VALUES("547","","","0","","0","","");
+INSERT INTO subject VALUES("548","?En7?Li?b??/?S???e???????\0\0","","0","","0","","");
+INSERT INTO subject VALUES("549","??e?.???|","???H?","0","?^?$j=?GW???)?E?+&","0","","");
+INSERT INTO subject VALUES("550","P?hZ?R????????M?@?B?iEJT?9","??r?gf??+W&!)O[A?R9@$???Q+?;?}t9@R?4??????~????b?????? V*?)??????3?½1	Vp)?R(?	?MXi???L?\0?8???c:\"h?]??]???z`?D_?&??????F?l3??1k0O?O??","0","","0","","");
+INSERT INTO subject VALUES("551","","","0","","0","","");
+INSERT INTO subject VALUES("552","","","0","","0","","");
+INSERT INTO subject VALUES("553","","","0","","0","","");
+INSERT INTO subject VALUES("554","u?????!aV?VX4=",".k??T??\0jEV`??`??","0","","0","","");
+INSERT INTO subject VALUES("555","","","0","","0","","");
+INSERT INTO subject VALUES("556","","","0","","0","","");
+INSERT INTO subject VALUES("557","?]{~\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0","\0\0V4DM\0\0\0\0\0\0\0?","0","","0","","");
+INSERT INTO subject VALUES("558","\0\0\0\0\0\0\0\0\0\0\0","\0\0\0\0\0\0\0\0\0\0\0","0","","0","","");
+INSERT INTO subject VALUES("559","","","0","","0","","");
+INSERT INTO subject VALUES("560","","","0","","0","","");
+INSERT INTO subject VALUES("561","","","0","","0","","");
+INSERT INTO subject VALUES("562","","","0","","0","","");
+INSERT INTO subject VALUES("563","","","0","","0","","");
+INSERT INTO subject VALUES("564","","","0","","0","","");
+INSERT INTO subject VALUES("565","","","0","","0","","");
+INSERT INTO subject VALUES("566","","","0","","0","","");
+INSERT INTO subject VALUES("567","","","0","","0","","");
+INSERT INTO subject VALUES("568","","","0","","0","","");
+INSERT INTO subject VALUES("569","","","0","","0","","");
+INSERT INTO subject VALUES("570","","","0","","0","","");
+INSERT INTO subject VALUES("571","","","0","","0","","");
+INSERT INTO subject VALUES("572","","","0","","0","","");
+INSERT INTO subject VALUES("573","","","0","","0","","");
+INSERT INTO subject VALUES("574","","","0","","0","","");
+INSERT INTO subject VALUES("575","","","0","","0","","");
+INSERT INTO subject VALUES("576","","","0","","0","","");
+INSERT INTO subject VALUES("577","","","0","","0","","");
+INSERT INTO subject VALUES("578","","","0","","0","","");
+INSERT INTO subject VALUES("579","","","0","","0","","");
+INSERT INTO subject VALUES("580","??D\0\0\0???.??\0+","??\0\0?\0\0\0	\0\0\0\0\0\0P\0\0\0\0\0\0X\0\0\0\0\0\0d\0\0\0\0\0\0l\0\0\0\0\0\0t\0\0\0\0\0\0|\0\0\0\0\0\0?\0\0\0\0\0\0?\0\0\0\0\0\0?\0\0\0\0\0\0?\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0Sheet1\0\0\0\0\0\0\0\0\0\0\0\0Worksheets\0\0\0\0\0\0\0\0\0t\0\0\0\0\0\0\0\0\0\0 \0\0\0\0\0\09\0\0\0\0\0\0A\0\0\0\0\0\0\0\0\0\0\0\0WorkbookG","0","","0","","");
+INSERT INTO subject VALUES("581","\0\0\0????.\0\0\0/\0\0\00\0\0\01\0\0\02\0\0\03\0\0","","0","","0","","");
+INSERT INTO subject VALUES("582","Subject Code","Subject Description","0","Pre Requisite","0","AY (Academic Year)","Semester");
+INSERT INTO subject VALUES("583","111234","sadasdsa","3","asdasdsa","12451","2021","1");
+INSERT INTO subject VALUES("584","121245","asdasdas","3","dasdas","12451","2342","4");
+INSERT INTO subject VALUES("585","1234412","asdsdas","3","dasdas","23412","1234","5");
+
+
+
---
+++ backup/index.php
@@ -0,0 +1,4 @@
+
+<a href="backup.php">Backup</a>
+<br><br><hr><br>
+<a href="restore.php">Restore</a>
