c72b1171ad9453d44b38285d4e2eef0008af6877
Ched28 <75325713+Ched28@users.noreply.github.com>
2021-12-02 15:33:57 +0800

    xxxxx

--- admin/sections.php
+++ admin/sections.php
@@ -4,5 +4,28 @@ include_once("config/dbcon.php");
 
 ?>
 <div class="content">
-    
+    <div class="section-dashboard">
+    <div class="headsec">
+        <h1> CAMPUS </h1>
+    </div>
+    <div class="secbody secbody-campus">
+        <?php 
+            $selectcampus = "SELECT * FROM campus";
+            $run_campus = mysqli_query($con, $selectcampus);
+            if($run_campus){
+                if($run_campus && mysqli_num_rows($run_campus) > 0 ){
+                    while($row = mysqli_fetch_array($run_campus))
+                    {
+                        $campusname = $row['campus_name'];
+                        $campuscode = $row['campus_code'];
+
+                        echo "<div><a href='campus_course.php?campcode=$campuscode'><h2> $campusname</h2></a></div>";
+
+                    }
+                }
+            }
+        ?>
+        
+    </div>
+    </div>
 </div>
--- admin/select_status.php
+++ admin/select_status.php
@@ -6,9 +6,21 @@ include_once("config/enc_dec.php");
 $id = $_GET['id'];
 $dec = qcu_decrypt($id);
 
-function getYear($con, $campuscode){
+function selectcourse($con, $firstcourse){
+    $select_course = "SELECT * FROM `course` WHERE coursename = '$firstcourse';";
+            $run_course = mysqli_query($con, $select_course);
+            if($run_course){
+                if($run_course && mysqli_num_rows($run_course) > 0){
+                    while($row2 = mysqli_fetch_array($run_course)){
+                        $courseshort = $row2['courseshort'];
 
+                        return $courseshort;
+                    }
+                }
+            }
+  
 }
+
 $select_status = "SELECT studentapprovals.StudentID, `studentinfo`.`FullName-Last`, `studentinfo`.`FullName-First`, `studentinfo`.`FullName-Middle`, studentenrollmentinfo.category, studentenrollmentinfo.firstcourse, studentenrollmentinfo.campus, student_sections.sectionname FROM studentapprovals INNER JOIN studentinfo ON studentapprovals.StudentID = studentinfo.StudentID INNER JOIN studentenrollmentinfo ON studentinfo.StudentID = studentenrollmentinfo.StudentID INNER JOIN student_sections ON studentenrollmentinfo.StudentID = student_sections.StudentID WHERE studentapprovals.StudentID = '$dec'";
 $run_select = mysqli_query($con, $select_status);
 if($run_select){
@@ -19,11 +31,14 @@ if($run_select){
             $firstname = $row['FullName-First'];
             $middlename = $row['FullName-Middle'];
             $category = $row['category'];
-            $c
-        }
-    }
-}
-?>
+            $campus = $row['campus'];
+            $sectionname = $row['sectionname'];
+            $firstcourse = $row['firstcourse'];
+            $course = selectcourse($con, $firstcourse);
+            $year = substr($sectionname, 5,1);
+            $code = substr($sectionname, 6,1);
+
+            ?>
 <div class="content">
     <div class="approval-form">
         <div class="headapp">
@@ -33,37 +48,37 @@ if($run_select){
                 <table style="padding: 2em;">
                     <tr>
                         <td colspan="2"> <h1> Student ID: </h1></td>
-                        <td colspan="2"> <h1> 21-0001</h1></td>
+                        <td colspan="2"> <h1> <?php echo $stid; ?></h1></td>
                     </tr>
                     <tr>
                         <td>  LAST NAME:</td>
-                        <td>  </td>
+                        <td>  <?php echo $lastname;?></td>
                         <td>  SECTION:</td>
-                        <td> </td>
+                        <td> <a href="select_sections.php?sec=<?php echo $sectionname;?>"><?php echo $sectionname;?></a></td>
                         
                     </tr>
                     <tr>
                         <td>  FIRST NAME:</td>
-                        <td>  </td>
+                        <td>  <?php echo $firstname;?></td>
                         <td>  CATEGORY:</td>
-                        <td> </td>
+                        <td>   <?php echo $category; ?></td>
                     </tr>
                     <tr>
                         <td>  MIDDLE NAME:</td>
-                        <td>  </td>
+                        <td>  <?php echo $middlename;?></td>
                         <td>  CAMPUS:</td>
-                        <td> </td>
+                        <td> <?php echo $campus;?></td>
                         
                     </tr>
                     <tr>
                     <td>  YEAR:</td>
-                        <td> </td>
+                        <td> <?php echo "$year$code";?></td>
                     <td>  COURSE:</td>
-                        <td> </td>
+                        <td> <?php echo $course;?></td>
                     </tr>
                     <tr>
                         <td> REGFORM</td>
-                        <td> <a href=""> VIEW PDF</a></td>
+                        <td> <a href=""> CREATE PDF</a></td>
                         <td> SEND EMAIL</td>
                         <td> <a href=""> SEND </a></td>
                     </tr>
@@ -73,3 +88,8 @@ if($run_select){
         </div>
     </div>
 </div>
+<?php
+}
+    }
+}
+?>
